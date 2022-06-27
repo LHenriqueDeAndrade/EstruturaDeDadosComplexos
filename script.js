f6779ffc8adf92b6b4a0f12dbcae4979c5c06e8f
LHenriqueDeAndrade <henriquesilvahenrique-2014@outlook.com>
2022-06-27 15:42:41 -0300

    atualizado

--- script.js
+++ script.js
@@ -25,43 +25,43 @@ let cadastro = [
     nome: "Paola",
     idade: 35,
     telefone: 33450541,
-    amigos: "Renan"
+    amigos: ['Renato', 'Cauã', 'Bianca', 'Santana']
   },
 
   {
     nome: "Lopez",
     idade: 28,
     telefone: 33460733,
-    amigos: "Fernando"
+    amigos: ['|Fernando', 'Carla', 'Rafner', 'Nadir']
   },
 
   {
     nome: "Vera",
     idade: 42,
     telefone: 33483274,
-    amigos: "Julio"
+    amigos: ['Julio', 'Juliana', 'Lais', 'Matheus']
   },
 
   {
     nome: "Mario",
     idade: 50,
     telefone: 33579412,
-    amigos: "Augusto"
+    amigos: ['Augusto', 'Sandra', 'Givaldo', 'Marcelo']
   },
 
   {
     nome: "Nicole",
     idade: 18,
     telefone: 33443274,
-    amigos: "Fabricio"
+    amigos: ['Fabricio', 'Carlos', 'Luan', 'Pedro']
   },
 ];
 
 // Mostre no console o nome de um amigo de cada lista.
 
 console.log(cadastro)
-console.log(cadastro[0].amigos)
-console.log(cadastro[1].amigos)
-console.log(cadastro[2].amigos)
-console.log(cadastro[3].amigos)
-console.log(cadastro[4].amigos)
+console.log(cadastro[0].amigos[0])
+console.log(cadastro[1].amigos[1])
+console.log(cadastro[2].amigos[3])
+console.log(cadastro[3].amigos[2])
+console.log(cadastro[4].amigos[0])
